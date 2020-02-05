7b3da1af82b09dccdfa9f6cc8706492e91023d9c
Jeff Peng <jeffpengyj@hotmail.com>
2020-02-05 14:32:53 -0500

    sync with Phase1

--- keywordTest/testAll.sh
+++
@@ -1,6 +0,0 @@
-for file in $(find ./ -name '*.pt'); 
-	do
-		fileOut=$(ssltrace "ptc -o1 -t1 -L ../lib/pt $file" ../lib/pt/scan.def -e | head -n -2)
-		printf "The test file $file yeilded:\n %s \n\n" "${fileOut}"
-done
-
