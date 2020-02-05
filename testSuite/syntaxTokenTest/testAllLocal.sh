e39f132f04ed8f34362db759aab13bc5041a9b30
ar4bid <14aaw4@queensu.ca>
2020-02-05 14:59:21 -0500

    Fixed printf in all test files

--- testSuite/syntaxTokenTest/testAllLocal.sh
+++ testSuite/syntaxTokenTest/testAllLocal.sh
@@ -1,6 +1,6 @@
 for file in $(find ./ -name '*.pt'); 
 	do
 		fileOut=$(ssltrace "ptc -o1 -t1 -L ../../lib/pt $file" ../../lib/pt/scan.def -e | head -n -2)    
-		printf "The test file $file yeilded:\n $fileOut\n\n"
+		printf "The test file $file yeilded:\n %s \n\n" "$fileOut"
 done
 
