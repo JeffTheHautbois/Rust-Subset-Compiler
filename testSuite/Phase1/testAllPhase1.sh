e1ebd9669eff3ee3c1ac1b69ed1064b218cde48b
ar4bid <14aaw4@queensu.ca>
2020-02-06 18:59:05 -0500

    Moved phase1 documentation and testing into sub-dirs

---
+++ testSuite/Phase1/testAllPhase1.sh
@@ -0,0 +1,10 @@
+cd characterClassesTest 
+./testAllLocal.sh
+cd ../syntaxTokenTest
+./testAllLocal.sh
+cd ../commentsTest
+./testAllLocal.sh
+cd ../keywordTest
+./testAllLocal.sh
+cd ../stringLiteralTest
+./testAllLocal.sh
