d6e48e1dcd9036f562dab90f2741e91dcf36e73a
ar4bid <14aaw4@queensu.ca>
2020-02-25 11:55:17 -0500

    Revert "Added module tests"
    This reverts commit fcfa1df3d7e3c974ba6263009f86be9de3a55409.

--- testSuite/Phase2/moduleTest/testAllLocal.sh
+++
@@ -1,6 +0,0 @@
-for file in $(find ./ -name '*.pt'); 
-	do
-		fileOut=$(ssltrace "ptc -o2 -t2 -L ../../../lib/pt $file" ../../../lib/pt/parser.def -e)    
-		printf "The test file $file yeilded:\n %s \n\n" "$fileOut"
-done
-
