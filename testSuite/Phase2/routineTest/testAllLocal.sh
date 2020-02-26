dddde94a2d7cf223c6099c400b8a5368e85b8bfb
ar4bid <14aaw4@queensu.ca>
2020-02-26 11:58:16 -0500

    Updated tests for match, module, routine and program

--- testSuite/Phase2/routineTest/testAllLocal.sh
+++ testSuite/Phase2/routineTest/testAllLocal.sh
@@ -1,6 +1,6 @@
 for file in $(find ./ -name '*.pt'); 
 	do
-		fileOut=$(ptc -o2 -t2 -L ../../../lib/pt $file)    
+		fileOut=$(ssltrace "ptc -o2 -t2 -L ../../../lib/pt $file" ../../../lib/pt/parser.def -e)    
 		printf "The test file $file yeilded:\n %s \n\n" "$fileOut"
 done
 
