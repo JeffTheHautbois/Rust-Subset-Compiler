0329da6fdd252e2f01b0ae91f00d54e5eee5477b
ar4bid <14aaw4@queensu.ca>
2020-04-01 17:42:58 -0400

    Added some test documentation and a Phase3 changeDocumentation dir

--- testSuite/Phase2/routineTest/testAllLocal.sh
+++ testSuite/Phase2/routineTest/testAllLocal.sh
@@ -1,6 +1,6 @@
 for file in $(find ./ -name '*.pt'); 
 	do
-		fileOut=$(ssltrace "ptc -o2 -t2 -L ../../../lib/pt $file" ../../../lib/pt/parser.def -e)    
+		fileOut=$(ssltrace "ptc -o2 -t2 -L ../../../ptsrc/lib/pt $file" ../../../ptsrc/lib/pt/parser.def -e)    
 		printf "The test file $file yeilded:\n %s \n\n" "$fileOut"
 done
 
