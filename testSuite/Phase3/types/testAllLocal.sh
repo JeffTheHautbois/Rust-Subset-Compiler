8f418ff08a4a836b4389eef6c7de278d268fb611
ar4bid <14aaw4@queensu.ca>
2020-04-02 00:50:59 -0400

    Added eOutputs and moved some dirs around

--- testSuite/Phase3/types/testAllLocal.sh
+++ testSuite/Phase3/types/testAllLocal.sh
@@ -1,6 +1,6 @@
 for file in $(find ./ -name '*.pt'); 
 	do
-		fileOut=$(ptc -o3 -t3 -L ../../../ptsrc/lib/pt $file)    
+		fileOut=$(ssltrace "ptc -o3 -t3 -L ../../../ptsrc/lib/pt $file" ../../../ptsrc/lib/pt/semantic.def -e)    
 		printf "The test file $file yeilded:\n %s \n\n" "$fileOut"
 done
 
